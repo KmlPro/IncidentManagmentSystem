99315af9def89746b04fbf7c32968bfc42a65139
kbytner <kamil.bytner@hotmail.com>
2020-02-02 01:18:25 +0100

    infrastructure tests project. First test for creating database

---
+++ src/IncidentManagmentSystem/IncidentReport.Infrastructure.IntegrationTests/IncidentReportDbContextTests.cs
@@ -0,0 +1,24 @@
+using System;
+using BuildingBlocks.Infrastructure;
+using IncidentReport.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using NUnit.Framework;
+
+namespace IncidentReport.Infrastructure.IntegrationTests
+{
+    public class IncidentReportDbContextTests
+    {
+        [Test]
+        public void EnsureDatabaseCreated_CreatedSucessfuly()
+        {
+            var options = new DbContextOptionsBuilder<IncidentReportDbContext>()
+                .ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            var context = new IncidentReportDbContext(options);
+
+            context.Database.EnsureCreated();
+        }
+    }
+}
