88fa710dfb0c7e4fdc47ec253059da6ce6c6298f
kbytner <kamil.bytner@hotmail.com>
2020-06-24 23:33:01 +0200

    removed automammper for public domain

--- src/IncidentManagmentSystem/IncidentReport.PublicDomain/DraftApplications/DraftApplicationDto.cs
+++ src/IncidentManagmentSystem/IncidentReport.PublicDomain/DraftApplications/DraftApplicationDto.cs
@@ -1,13 +1,9 @@
 using System;
 using System.Collections.Generic;
-using AutoMapper;
-using IncidentReport.Domain.IncidentVerificationApplications;
-using IncidentReport.PublicDomain.Configuration;
-using IncidentReport.PublicDomain.Configuration.Converters;
 
 namespace IncidentReport.PublicDomain.DraftApplications
 {
-    public class DraftApplicationDto : IMapFrom<DraftApplication>
+    public class DraftApplicationDto
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
@@ -16,16 +12,5 @@ namespace IncidentReport.PublicDomain.DraftApplications
         public List<Guid> SuspiciousEmployees { get; set; }
         public Guid ApplicantId { get; set; }
         public List<AttachmentDto> Attachments { get; set; }
-
-        public void Mapping(Profile profile)
-        {
-            profile.CreateMap<List<Attachment>, List<AttachmentDto>>().ConvertUsing(new AttachmentsConverter());
-
-            profile.CreateMap<DraftApplication, DraftApplicationDto>()
-                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id.Value))
-                .ForMember(d => d.Title, opt => opt.MapFrom(s => s.ContentOfApplication.Title))
-                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.ContentOfApplication.Description))
-                .ForMember(d => d.ApplicantId, opt => opt.MapFrom(s => s.ApplicantId.Value));
-        }
     }
 }
---
+++ src/IncidentManagmentSystem/IncidentReport.PublicDomain/Employees/EmployeeDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IncidentReport.PublicDomain.Employees
+{
+    public class EmployeeDto
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Surname { get; }
+    }
+}
