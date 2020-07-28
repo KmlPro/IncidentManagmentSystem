46523e398e313420d942bd5320a77a16d5591740
KmL <kamil.bytner@gmai.com>
2020-07-29 00:25:59 +0200

    comment on Mapping profile

--- src/IncidentManagmentSystem/IncidentReport.Infrastructure.ReadModels/Dtos/DraftApplications/DraftApplicationProfile.cs
+++ src/IncidentManagmentSystem/IncidentReport.Infrastructure.ReadModels/Dtos/DraftApplications/DraftApplicationProfile.cs
@@ -1,6 +1,5 @@
 using AutoMapper;
 using IncidentReport.ReadModels.DbEntities;
-using IncidentReport.ReadModels.Dtos.Employees;
 
 namespace IncidentReport.ReadModels.Dtos.DraftApplications
 {
@@ -8,12 +7,11 @@ namespace IncidentReport.ReadModels.Dtos.DraftApplications
     {
         public DraftApplicationProfile()
         {
-            this.CreateMap<DraftApplication, DraftApplicationDto>().
-                ForMember(
+            //kbytner 29.07.2020 - to do mapping for suspiciousEmployee and Attachments
+            this.CreateMap<DraftApplication, DraftApplicationDto>().ForMember(
                 dest => dest.IncidentType,
                 opt => opt.MapFrom(src => src.IncidentTypeValue)
             );
-
         }
     }
 }
