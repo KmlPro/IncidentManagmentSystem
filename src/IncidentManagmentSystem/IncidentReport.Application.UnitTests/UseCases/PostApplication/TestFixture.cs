c9aa452f62b6f1fa6cef4736b11c799ef287c44e
KmL <kamil.bytner@gmai.com>
2020-08-07 00:36:01 +0200

    some refactoring in test fixture classes

--- src/IncidentManagmentSystem/IncidentReport.Application.UnitTests/UseCases/PostApplication/TestFixture.cs
+++ src/IncidentManagmentSystem/IncidentReport.Application.UnitTests/UseCases/PostApplication/TestFixture.cs
@@ -1,7 +1,26 @@
+using IncidentReport.Application.Boundaries.PostApplicationUseCase;
+using IncidentReport.Application.UnitTests.Factories;
+using IncidentReport.Domain.IncidentVerificationApplications.DraftApplications;
+
 namespace IncidentReport.Application.UnitTests.UseCases.PostApplication
 {
     public class TestFixture
     {
-        
+        public PostApplicationInput CreateInputWithoutDraft()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PostApplicationInput CreateInputWithDraft(DraftApplication draftApplication)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DraftApplication PrepareDraftApplication(IDraftApplicationRepository draftApplicationRepository)
+        {
+            var draftApplication = DraftApplicationFactory.Create();
+            draftApplicationRepository.Create(draftApplication);
+            return draftApplication;
+        }
     }
 }
--- src/IncidentManagmentSystem/IncidentReport.Application.UnitTests/UseCases/PostApplication/ValidPath_PostApplicationUseCase.cs
+++ src/IncidentManagmentSystem/IncidentReport.Application.UnitTests/UseCases/PostApplication/ValidPath_PostApplicationUseCase.cs
@@ -1,14 +1,57 @@
+using System.Threading;
 using System.Threading.Tasks;
+using BuildingBlocks.Application.UnitTests;
+using IncidentReport.Application.UnitTests.Mocks;
+using IncidentReport.Application.UseCases;
+using IncidentReport.Domain.IncidentVerificationApplications.DraftApplications;
+using IncidentReport.Domain.IncidentVerificationApplications.IncidentApplications;
 using NUnit.Framework;
 
 namespace IncidentReport.Application.UnitTests.UseCases.PostApplication
 {
+    //kbytner 06.08.2020- to do, complete tests with implementation
     public class ValidPath_PostApplicationUseCase : BaseTest
     {
+        private TestFixture _testFixture;
+        private IDraftApplicationRepository _draftApplicationRepository;
+        private IIncidentApplicationRepository _incidentApplicationRepository;
+
+        public ValidPath_PostApplicationUseCase()
+        {
+            this._testFixture = new TestFixture();
+            this._draftApplicationRepository = new MockDraftApplicationRepository();
+            this._incidentApplicationRepository = new MockIncidentApplicationRepository();
+        }
+
         [Test]
         public async Task WithoutDraftApplication_PostedSuccessfully()
         {
+            var useCase = this._testFixture.CreateInputWithoutDraft();
+            var handler = new PostApplicationUseCase(this._incidentApplicationRepository,
+                this._draftApplicationRepository, this.CurrentUserContext, this.IFileStorageService,
+                new PostApplicationUseCaseOutputPort());
 
+            var useCaseOutput =
+                (PostApplicationUseCaseOutputPort)await handler.Handle(useCase, new CancellationToken());
+
+            Assert.AreEqual(OutputPortInvokedMethod.Standard, useCaseOutput.InvokedOutputMethod);
+        }
+
+        [Test]
+        public async Task WithDraftApplication_PostedSuccessfully()
+        {
+            var draftApplication =
+                this._testFixture.PrepareDraftApplication(this._draftApplicationRepository);
+
+            var useCase = this._testFixture.CreateInputWithDraft(draftApplication);
+            var handler = new PostApplicationUseCase(this._incidentApplicationRepository,
+                this._draftApplicationRepository, this.CurrentUserContext, this.IFileStorageService,
+                new PostApplicationUseCaseOutputPort());
+
+            var useCaseOutput =
+                (PostApplicationUseCaseOutputPort)await handler.Handle(useCase, new CancellationToken());
+
+            Assert.AreEqual(OutputPortInvokedMethod.Standard, useCaseOutput.InvokedOutputMethod);
         }
     }
 }
